9ed3611d647798ecde114a19a1db00cfd4e148e3
ZovuyoMbekeni <159232994+ZovuyoMbekeni@users.noreply.github.com>
2025-08-29 20:23:27 +0200

    JobSeeker Done !

---
+++ Job-Recruitment-Portal/src/main/java/za/ac/cput/service/JobSeekerService.java
@@ -0,0 +1,28 @@
+package za.ac.cput.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.JobListing;
+import za.ac.cput.domain.JobSeeker;
+import za.ac.cput.repository.JobSeekerRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class JobSeekerService {
+    @Autowired
+    private JobSeekerRepository jobSeekerRepository;
+
+    public Optional<JobSeeker> getJobSeekerById(Long id) {
+        return jobSeekerRepository.findById(id);
+    }
+
+    public List<JobSeeker> getAllJobSeekers() {return jobSeekerRepository.findAll();}
+
+    public void deleteJobSeeker(Long id) {jobSeekerRepository.deleteById(id);}
+
+    public JobSeeker saveJobSeeker(JobSeeker jobSeeker) {
+        return null;
+    }
+}
